c819244010585a131d50329ebbf13bf9c50802bd
Guillaume Fouilleul <guillaume.fouilleul@ni.com>
2024-03-12 17:24:03 +0100

    Add daisy chain SPI slave with PIO

---
+++ Live_timing_script/PicoFirmware/pio_spi.py
@@ -0,0 +1,30 @@
+import rp2
+from machine import Pin
+
+# Pinout definition
+pin_cs = 1
+pin_sck = 2
+pin_mosi = 4
+pin_miso = 3
+
+@rp2.asm_pio()
+def spi_slave():
+    wrap_target()
+    wait(0, gpio, pin_cs)   # Wait for CS = 0
+    label("daisy_chain")
+    wait(0, gpio, pin_sck)  # SCK Falling Edge
+    mov(osr, isr)           # IRS -> OSR
+    out(pins, 1)            # OSR -> MISO
+    wait(1, gpio, pin_sck)  # SCK Rising Edge
+    in_(pins, 1)            # MOSI -> ISR
+    jmp(pin, "end")         # Jump if CS = 1
+    jmp("daisy_chain")
+    label("end")
+    push()                  # IRS -> RX FIFO
+    irq(0)                  # IRQ SET 0
+    wrap()
+
+
+sm = rp2.StateMachine(0, spi_slave, out_base=Pin(pin_miso), in_base=Pin(pin_mosi), jmp_pin=Pin(pin_cs))
+sm.irq(lambda p: print(f"SPI RX: {sm.get():08X}"))
+sm.active(1)
